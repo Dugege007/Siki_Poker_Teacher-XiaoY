29773625dd21f88e80be8c5f1942a109953e9f26
Dugege007 <351080175@qq.com>
2023-07-26 20:45:46 +0800

    21-Player玩家管理

---
+++ PokerServer/Player/Player.cs
@@ -0,0 +1,36 @@
+﻿#nullable disable
+public class Player
+{
+    /// <summary>
+    /// 玩家 ID
+    /// </summary>
+    public string id = "";
+
+    /// <summary>
+    /// 玩家对应的客户端
+    /// </summary>
+    public ClientState state;
+
+    /// <summary>
+    /// 玩家数据
+    /// </summary>
+    public PlayerData data;
+
+    /// <summary>
+    /// 构造函数
+    /// </summary>
+    /// <param name="state">玩家对应的客户端</param>
+    public Player(ClientState state)
+    {
+        this.state = state;
+    }
+
+    /// <summary>
+    /// 封装的发送消息
+    /// </summary>
+    /// <param name="msgBase">消息</param>
+    public void Send(MsgBase msgBase)
+    {
+        NetManager.Send(state, msgBase);
+    }
+}
---
+++ PokerServer/Player/PlayerManager.cs
@@ -0,0 +1,55 @@
+﻿#nullable disable
+public class PlayerManager
+{
+    /// <summary>
+    /// 在线玩家字典
+    /// </summary>
+    private static Dictionary<string, Player> players = new Dictionary<string, Player>();
+
+    /// <summary>
+    /// 玩家是否在线
+    /// </summary>
+    /// <param name="id">玩家 ID</param>
+    /// <returns></returns>
+    public static bool IsOnLine(string id)
+    {
+        return players.ContainsKey(id);
+    }
+
+    /// <summary>
+    /// 根据 ID 获取对应的玩家
+    /// </summary>
+    /// <param name="id">玩家 ID</param>
+    /// <returns></returns>
+    public static Player GetPlayer(string id)
+    {
+        if (players.ContainsKey(id))
+        {
+            return players[id];
+        }
+        else
+        {
+            Console.WriteLine("未获取到玩家");
+            return null;
+        }
+    }
+
+    /// <summary>
+    /// 增添玩家
+    /// </summary>
+    /// <param name="id">玩家 ID</param>
+    /// <param name="player">玩家</param>
+    public static void AddPlayer(string id, Player player)
+    {
+        players.Add(id, player);
+    }
+
+    /// <summary>
+    /// 根据 ID 删除玩家
+    /// </summary>
+    /// <param name="id">玩家 ID</param>
+    public static void RemovePlayer(string id)
+    {
+        players.Remove(id);
+    }
+}
