fc7ef81eddc709615bbd16b2c29e85f198d2c3db
Dugege007 <351080175@qq.com>
2023-07-27 16:11:11 +0800

    22-创建MySQL数据库
    使用Nevicat创建Mysql数据库
    注意：检查Mysql80服务是否启用，以免出现连接不上的报错

---
+++ PokerServer/DataBase/DBManager.cs
@@ -0,0 +1,38 @@
+﻿using MySqlConnector;
+
+#nullable disable
+public class DBManager
+{
+    /// <summary>
+    /// 数据库对象
+    /// </summary>
+    public static MySqlConnection mysql;
+
+    /// <summary>
+    /// 连接数据库
+    /// </summary>
+    /// <param name="db">数据表</param>
+    /// <param name="ip">IP 地址</param>
+    /// <param name="port">端口号</param>
+    /// <param name="user">用户名</param>
+    /// <param name="pw">密码</param>
+    /// <returns></returns>
+    public static bool Connect(string db, string ip, int port,string user, string pw)
+    {
+        mysql = new MySqlConnection();
+        string s = string.Format("Database = {0}; Data Source = {1}; Port = {2}; User = {3}; Password = {4};", db, ip, port, user, pw);
+        mysql.ConnectionString = s;
+
+        try
+        {
+            mysql.Open();
+            Console.WriteLine("[数据库] 启动成功");
+            return true;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("[数据库] 启动失败：" + ex.Message);
+            return false;
+        }
+    }
+}
--- PokerServer/MainClass.cs
+++ PokerServer/MainClass.cs
@@ -7,6 +7,9 @@
         /// </summary>
         private static void Main()
         {
+            if (!DBManager.Connect("Game", "127.0.0.1", 3306, "root", "dhb351080175"))
+                return;
+
             // 连接到本地服务器的 8888 端口
             NetManager.Connect("127.0.0.1", 8888);
         }
