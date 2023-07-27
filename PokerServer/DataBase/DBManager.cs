e10539905745962388cc15bff94c9655085fad70
Dugege007 <351080175@qq.com>
2023-07-27 18:24:19 +0800

    26-数据库 检查密码和获取角色信息
    操作流程：
    - 玩家点击 注册
    - 玩家 登录，检查账号密码
    - 如果正确，获取玩家数据

--- PokerServer/DataBase/DBManager.cs
+++ PokerServer/DataBase/DBManager.cs
@@ -51,6 +51,8 @@ public class DBManager
     private static bool IsSafeString(string str)
     {
         return !Regex.IsMatch(str, @"[-|;|,|\/|\[|\]|\{|\}|%|@|\*|!|\']");
+
+        //TODO 可以加入更多的防止 SQL 注入攻击规则
     }
 
     /// <summary>
@@ -169,6 +171,96 @@ public class DBManager
             // 如果插入操作失败，打印错误信息
             Console.WriteLine("[数据库] 创建角色失败 " + ex.Message);
             return false;
+        }
+    }
+
+    /// <summary>
+    /// 检查用户 ID 和密码是否匹配
+    /// </summary>
+    /// <param name="id">用户 ID</param>
+    /// <param name="pw">用户密码</param>
+    /// <returns>如果 ID 和密码匹配，返回 true；否则，返回 false</returns>
+    public static bool CheckPassword(string id, string pw)
+    {
+        // 检查用户输入是否安全，防止 SQL 注入攻击
+        if (!IsSafeString(id))
+        {
+            Console.WriteLine("[数据库] 检查 ID 失败，ID 不安全");
+            return false;
+        }
+
+        if (!IsSafeString(pw))
+        {
+            Console.WriteLine("[数据库] 检查密码失败，密码不安全");
+            return false;
+        }
+
+        // 创建 SQL 查询语句，用于在 account 表中查找匹配的 ID 和密码
+        string s = string.Format("SELECT * FROM account WHERE id = '{0}', pw = '{1}'", id, pw);
+        try
+        {
+            // 创建一个新的 MySQL 命令对象
+            MySqlCommand cmd = new MySqlCommand(s, mysql);
+            // 执行查询并获取结果
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+            // 检查查询结果是否包含任何行（如果包含，说明 ID 和密码匹配）
+            bool hasRows = dataReader.HasRows;
+            dataReader.Close();
+            return hasRows;
+        }
+        catch (Exception ex)
+        {
+            // 如果查询操作失败，打印错误信息
+            Console.WriteLine("[数据库] 检查密码失败 " + ex.Message);
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// 获取指定 ID 的玩家数据
+    /// </summary>
+    /// <param name="id">玩家角色 ID</param>
+    /// <returns></returns>
+    public static PlayerData GetPlayerData(string id)
+    {
+        // 检查用户输入是否安全，防止 SQL 注入攻击
+        if (!IsSafeString(id))
+        {
+            Console.WriteLine("[数据库] 获取角色信息失败，ID 不安全");
+            return null;
+        }
+
+        // 创建 SQL 查询语句，用于在 player 表中查找指定 ID 的玩家数据
+        string s = string.Format("SELECT * FROM player WHERE id = '{0}'", id);
+
+        try
+        {
+            // 创建一个新的 MySQL 命令对象
+            MySqlCommand cmd = new MySqlCommand(s, mysql);
+            // 执行查询并获取结果
+            MySqlDataReader dataReader = cmd.ExecuteReader();
+            // 检查查询结果是否包含任何行（如果包含，说明找到了玩家）
+            bool hasRows = dataReader.HasRows;
+            if (!hasRows)
+            {
+                dataReader.Close();
+                return null;
+            }
+
+            // 读取玩家数据
+            dataReader.Read();
+            string data = dataReader.GetString("data");
+
+            // 将 JSON 格式的字符串反序列化为 PlayerData 对象
+            PlayerData playerData = JsonConvert.DeserializeObject<PlayerData>(data);
+            dataReader.Close();
+            return playerData;
+        }
+        catch (Exception ex)
+        {
+            // 如果查询操作失败，打印错误信息
+            Console.WriteLine("[数据库] 获取角色信息失败 " + ex.Message);
+            return null;
         }
     }
 }
